4ccc5fa3ea44af55392ae5ffca1fcec9b69b383b
calujod <calujord@gmail.com>
2014-01-25 16:44:25 -0500

    formulario faltante

---
+++ django_mongodb/frontend/forms.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+# -*- coding: UTF-8-*-
+
+__author__ = 'calujod'
+
+from django import forms
+
+
+class PersonaForm(forms.Form):
+    nombre = forms.CharField(max_length=20, label=("Cédula, Ruc o pasaporte"))
+    apellido = forms.CharField(max_length=220, min_length=3, label=("Razón Social"))
+
+    def __init__(self,*args, **kwargs):
+        super(PersonaForm, self).__init__(*args, **kwargs)
+        self.fields['nombre'].widget.attrs['class'] = "form-control"
+        self.fields['apellido'].widget.attrs['class'] = "form-control"
+
+    def get_nombre(self):
+        return self.data["nombre"]
+    def get_apellido(self):
+        return self.data["apellido"]
